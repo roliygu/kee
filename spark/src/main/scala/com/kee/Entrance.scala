d5da003f85d9c2349f57909e88d83287b15ea7f2
Gu Rui <roliygu@gmail.com>
2017-11-26 17:16:17 +0800

    add month data

--- spark/src/main/scala/com/kee/Entrance.scala
+++ spark/src/main/scala/com/kee/Entrance.scala
@@ -1,6 +1,7 @@
 package com.kee
 
 import com.kee.data._
+import com.kee.utils.SparkUtils
 import org.apache.spark.mllib.regression.LinearRegressionWithSGD
 import org.apache.spark.storage.StorageLevel
 
@@ -13,7 +14,25 @@ object Entrance {
     }
 
     def main(args: Array[String]): Unit = {
-        statistics()
+
+        //        import com.kee.utils.SparkUtils.sqlContext.implicits._
+        //
+        //        val users = DataDescription.loadAllUser().filter(_.id.split("_")(1).toInt == 11).cache()
+        //        val loanSum = DataDescription.loadLoanSum().cache()
+        //
+        ////        val usersCount = users.count()
+        //        val loanSumCount = loanSum.count()
+        //        println("a")
+        //
+        //        val different = users.map(e => (e.uid, e)).rdd.join(
+        //            loanSum.map(e => (e.uid, e)).rdd
+        //        ).map {
+        //            case (uid, (user, loanSumOpt)) =>
+        //               s"${uid}\t${user.loanSum.loanSum}\t${loanSumOpt.loanSum}"
+        //        }.repartition(1).saveAsTextFile("./spark/diff")
+        //
+        //        println(different)
+
     }
 
     def train() = {
--- spark/src/main/scala/com/kee/data/DataDescription.scala
+++ spark/src/main/scala/com/kee/data/DataDescription.scala
@@ -6,7 +6,9 @@ import java.text.SimpleDateFormat
 import com.kee.utils.{HDFSUtils, SparkUtils}
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.sql.Dataset
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Dataset}
+import org.apache.spark.storage.StorageLevel
 
 /**
   *
@@ -16,7 +18,7 @@ import org.apache.spark.sql.Dataset
   * @param activeDate 激活日期
   * @param limit      初始额度
   */
-case class User(uid: Long, age: Int, sex: Int, activeDate: Date, limit: Double,
+case class User(var id: String, uid: Long, age: Int, sex: Int, activeDate: Date, limit: Double,
                 var clicks: Seq[Click], var orders: Seq[Order], var loans: Seq[Loan], var loanSum: LoanSum)
 
 /**
@@ -54,7 +56,7 @@ object DataDescription {
 
     import SparkUtils.sqlContext.implicits._
 
-    val PREFIX = "/Users/roliy/project/kee/data"
+    val PREFIX = "/Users/roliy/jdd_data"
     val USER_PATH = s"$PREFIX/user"
     val CLICK_PATH = s"$PREFIX/click"
     val ORDER_PATH = s"$PREFIX/order"
@@ -63,7 +65,9 @@ object DataDescription {
     val ALL_USER_PATH = s"$PREFIX/all_user"
     val NOV_FIRST = 1477929600000L // 2016-11-01
 
-    def convertUser() = {
+    val random: Int = (Math.random() * 1000).toInt
+
+    def convertUser(): DataFrame = {
         val sourcePath = "/Users/roliy/jdd_data/t_user.csv"
         HDFSUtils.deleteIfExist(USER_PATH)
         val df = SparkUtils.sparkContext.textFile(sourcePath)
@@ -74,14 +78,14 @@ object DataDescription {
                     val slices = e.split(",")
                     val dateSlices = slices(3).split("-").map(_.toInt)
                     val date = new Date(dateSlices(0) - 1900, dateSlices(1) - 1, dateSlices(2))
-                    User(slices(0).toLong, slices(1).toInt, slices(2).toInt, date, slices(4).toDouble,
+                    User("", slices(0).toLong, slices(1).toInt, slices(2).toInt, date, slices(4).toDouble,
                         Seq(), Seq(), Seq(), null)
                 }.toDF()
         df.write.parquet(USER_PATH)
-        df.show(10)
+        df
     }
 
-    def convertClick() = {
+    def convertClick(): DataFrame = {
         val sourcePath = "/Users/roliy/jdd_data/t_click.csv"
         HDFSUtils.deleteIfExist(CLICK_PATH)
         val df = SparkUtils.sparkContext.textFile(sourcePath)
@@ -99,10 +103,10 @@ object DataDescription {
                 }
                 .toDF
         df.write.parquet(CLICK_PATH)
-        df.show(10)
+        df
     }
 
-    def convertOrder() = {
+    def convertOrder(): DataFrame = {
         val sourcePath = "/Users/roliy/jdd_data/t_order.csv"
         HDFSUtils.deleteIfExist(ORDER_PATH)
         val df = SparkUtils.sparkContext.textFile(sourcePath)
@@ -117,10 +121,10 @@ object DataDescription {
                     Order(slices(0).toLong, date, price, slices(3).toInt, slices(4).toLong, slices(5).toDouble)
                 }.toDF()
         df.write.parquet(ORDER_PATH)
-        df.show(10)
+        df
     }
 
-    def covertLoan() = {
+    def convertLoan(): DataFrame = {
         val sourcePath = "/Users/roliy/jdd_data/t_loan.csv"
         HDFSUtils.deleteIfExist(LOAN_PATH)
         val df = SparkUtils.sparkContext.textFile(sourcePath)
@@ -137,10 +141,10 @@ object DataDescription {
                     }
                 }.toDF()
         df.write.parquet(LOAN_PATH)
-        df.show(10)
+        df
     }
 
-    def covertLoanSum() = {
+    def convertLoanSum(): DataFrame = {
         val sourcePath = "/Users/roliy/jdd_data/t_loan_sum.csv"
         HDFSUtils.deleteIfExist(LOAN_SUM_PATH)
         val df = SparkUtils.sparkContext.textFile(sourcePath)
@@ -152,7 +156,7 @@ object DataDescription {
                     LoanSum(slices(0).toLong, slices(2).toDouble)
                 }.toDF()
         df.write.parquet(LOAN_SUM_PATH)
-        df.show(10)
+        df
     }
 
     def loadUser(): Dataset[User] = SparkUtils.sqlContext.read.parquet(USER_PATH).as[User]
@@ -166,6 +170,108 @@ object DataDescription {
     def loadLoanSum(): Dataset[LoanSum] = SparkUtils.sqlContext.read.parquet(LOAN_SUM_PATH).as[LoanSum]
 
     def loadAllUser(): Dataset[User] = SparkUtils.sqlContext.read.parquet(ALL_USER_PATH).as[User]
+
+    def fillClick(_users: RDD[(Long, User)], _clicks: RDD[(Long, Click)], month: Int): RDD[(Long, User)] = {
+        val clicks = _clicks.filter(e => e._2.clickTime.getMonth < month)
+                .map(e => (e._1, Array(e._2)))
+                .reduceByKey((e1, e2) => {
+                    e1 ++ e2
+                })
+        _users.leftOuterJoin(clicks)
+                .map {
+                    case (uid, (user, _clicksOps)) =>
+                        val _click: Seq[Click] = _clicksOps match {
+                            case Some(v) => v
+                            case _ => user.clicks
+                        }
+                        user.clicks = _click
+                        (uid, user)
+                }
+    }
+
+    def fillOrder(_user: RDD[(Long, User)], _orders: RDD[(Long, Order)], month: Int): RDD[(Long, User)] = {
+        val orders = _orders.filter(_._2.buyDate.getMonth < month)
+                .map(e => (e._1, Array(e._2)))
+                .reduceByKey((e1, e2) => {
+                    e1 ++ e2
+                })
+        _user.leftOuterJoin(orders)
+                .map {
+                    case (uid, (user, _orderOps)) =>
+                        val _order: Seq[Order] = _orderOps match {
+                            case Some(v) => v
+                            case _ => user.orders
+                        }
+                        user.orders = _order
+                        (uid, user)
+                }
+    }
+
+    def fillLoans(_user: RDD[(Long, User)], _loans: RDD[(Long, Loan)], month: Int): RDD[(Long, User)] = {
+        val loans = _loans.filter(_._2.loanTime.getMonth < month)
+                .map(e => (e._1, Array(e._2)))
+                .reduceByKey((e1, e2) => {
+                    e1 ++ e2
+                })
+        _user.leftOuterJoin(loans)
+                .map {
+                    case (uid, (user, _loanOps)) =>
+                        val _loan: Seq[Loan] = _loanOps match {
+                            case Some(v) => v
+                            case _ => user.loans
+                        }
+                        user.loans = _loan
+                        (uid, user)
+                }
+    }
+
+    /**
+      * 将month月的loan聚集起来，作为loanSum
+      *
+      * @param _user
+      * @param _loans
+      * @param month
+      */
+    def fillLoanSum(_user: RDD[(Long, User)], _loans: RDD[(Long, Loan)], month: Int): RDD[(Long, User)] = {
+        val loanSum = _loans.filter(_._2.loanTime.getMonth == month)
+                .map(e => (e._1, LoanSum(e._1, e._2.loanAmount)))
+                .reduceByKey((e1, e2) => {
+                    LoanSum(e1.uid, e1.loanSum + e2.loanSum)
+                })
+        _user.leftOuterJoin(loanSum)
+                .map {
+                    case (uid, (user, _loanSumOps)) =>
+                        val _loanSum: LoanSum = _loanSumOps match {
+                            case Some(v) => v
+                            case _ => LoanSum(uid, 0.0)
+                        }
+                        user.loanSum = _loanSum
+                        (uid, user)
+                }
+    }
+
+    /**
+      * 过滤掉month之后(包括month)的行为数据，并拿month整月的loan做成loanSum
+      * @param rawUser
+      * @param rawClick
+      * @param rawOrder
+      * @param rawLoan
+      * @param _month  1-base 9表示9月
+      * @return
+      */
+    def joinData(rawUser: Dataset[User], rawClick: Dataset[Click], rawOrder: Dataset[Order], rawLoan: Dataset[Loan], _month: Int): RDD[User] = {
+        val month = _month - 1
+        val withClick = fillClick(rawUser.rdd.map(e => (e.uid, e)),
+            rawClick.rdd.map(e => (e.uid, e)), month)
+        val withOrder = fillOrder(withClick, rawOrder.rdd.map(e => (e.uid, e)), month)
+        val withLoan = fillLoans(withOrder, rawLoan.rdd.map(e => (e.uid, e)), month)
+        fillLoanSum(withLoan, rawLoan.rdd.map(e => (e.uid, e)), month)
+                .map(_._2)
+                .map{user=>
+                    user.id = s"${user.uid}_${_month}_${random}"
+                    user
+                }
+    }
 
     def joinData() = {
 
@@ -290,6 +396,26 @@ object DataDescription {
         Math.sqrt(predict.map { e =>
             (e._1 - e._2) * (e._1 - e._2)
         }.reduce(_ + _) / count)
+    }
+
+    def generateData(): Unit = {
+
+        import com.kee.utils.SparkUtils.sqlContext.implicits._
+
+        val rawUsers = convertUser().as[User].persist(StorageLevel.MEMORY_AND_DISK)
+        val rawClicks = convertClick().as[Click].persist(StorageLevel.MEMORY_AND_DISK)
+        val rawLoans = convertLoan().as[Loan].persist(StorageLevel.MEMORY_AND_DISK)
+        val rawOrders = convertOrder().as[Order].persist(StorageLevel.MEMORY_AND_DISK)
+        // convertLoanSum()
+
+        val month9 = DataDescription.joinData(rawUsers, rawClicks, rawOrders, rawLoans, 9).persist(StorageLevel.MEMORY_AND_DISK)
+        val month10 = DataDescription.joinData(rawUsers, rawClicks, rawOrders, rawLoans, 10).persist(StorageLevel.MEMORY_AND_DISK)
+        val month11 = DataDescription.joinData(rawUsers, rawClicks, rawOrders, rawLoans, 11).persist(StorageLevel.MEMORY_AND_DISK)
+
+        val path = s"${PREFIX}/all_user"
+        HDFSUtils.deleteIfExist(path)
+        month9.union(month10).union(month11).toDF.write.parquet(path)
+
     }
 
 }
